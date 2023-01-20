b59ca48174de48e89286331fe986209a72873e7c
Théo Sigaud <sigaudtheo@gmail.com>
2023-01-20 12:58:34 +0100

    Add security routes front

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,6 +4,7 @@ import CreateAdvertisementView from '../views/CreateAdvertisementView.vue'
 import MyListingsView from '../views/MyListingsView.vue'
 import MyAdvertisementsView from '../views/MyAdvertisementsView.vue'
 import ConfirmAccount from '../views/ConfirmView.vue'
+import jsCookie from 'js-cookie'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,8 @@ const router = createRouter({
     {
       path: '/my-listings',
       name: 'my-listings',
-      component: MyListingsView
+      component: MyListingsView,
+      meta: {requiresAuth: true}
     },
     {
       path: '/my-advertisement',
@@ -36,4 +38,29 @@ const router = createRouter({
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = jsCookie.get('jwt')
+
+    const requestToken = new Request(
+        "https://localhost/api/auth",
+        {
+          method: "POST",
+          headers: {
+            "Authorization": "Bearer " + token
+          }
+        });
+
+    fetch(requestToken)
+        .then((response) => {
+            if (response.status === 200) {
+                next()
+            } else {
+                next('/login')
+            }
+        })
+  }else {
+    next()
+  }
+})
 export default router
