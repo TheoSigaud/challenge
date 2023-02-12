68a4601aaa9ef5863c9b88f520dd8ea5606f8486
Théo Sigaud <sigaudtheo@gmail.com>
2023-02-12 15:26:07 +0100

    Chack age

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -16,10 +16,8 @@
   function register() {
     registerData.value.error = null
     registerData.value.success = null
-    console.log(registerData.value)
 
     const date = new Date(registerData.value.birthday);
-    console.log(date)
 
     const today = new Date();
     let age = today.getFullYear() - date.getFullYear();
@@ -41,10 +39,15 @@
       return
     }
 
-    if (age < 15 || age > 110) {
-      registerData.value.error = 'Vous devez avoir minimum 12 ans.'
+    if (registerData.value.password.length < 8) {
+      registerData.value.error = 'Le mot de passe doit contenir au moins 8 caractères.'
       return
     }
+
+    // if (age < 15 || age > 110) {
+    //   registerData.value.error = 'Vous devez avoir minimum 12 ans.'
+    //   return
+    // }
 
     const requestRegister = new Request(
         "https://localhost/api/users",
@@ -72,7 +75,18 @@
             registerData.value.success = 'Votre compte a bien été créé. Vérifiez vos mails pour confirmer votre compte.'
             registerData.value.error = null
           } else {
-            registerData.value.error = 'Les informations saisies sont incorrectes. Il se peut que l\'email soit déjà utilisé.'
+            return response.json()
+          }
+        })
+        .then(data => {
+          if (data.violations) {
+            if (data.violations[0]['propertyPath'] === 'birthday') {
+              registerData.value.error = 'Vous devez avoir minimum 12 ans.'
+            } else {
+              registerData.value.error = 'Les informations saisies sont incorrectes'
+            }
+          }else {
+            registerData.value.error = 'Cet email est déjà utilisé.'
           }
         })
   }
