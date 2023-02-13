65662532e167c1fcb59ca6f51d5046272cbb5562
Sidox <pro.sidox@gmail.com>
2023-02-13 23:43:12 +0100

    nearly working test and lower case

--- api/tests/Api/GreetingsTest.php
+++ api/tests/Api/GreetingsTest.php
@@ -12,9 +12,25 @@ class GreetingsTest extends ApiTestCase
 //            'name' => 'Kévin',
 //        ]]);
 
+// $object_2 = (new User())
+//             ->setEmail("rexrider75@gmail.com")
+//             ->setRoles(['ROLE_USER'])
+//             ->setPassword("testtest")
+//             ->setToken(bin2hex(random_bytes(32)))
+//             ->setAddress("4 rue de la paix")
+//             ->setBirthday(new \DateTime("1990-01-01"))
+//             ->setLastname("sidox")
+//             ->setFirstname("sidox");
+
         static::createClient()->request('POST', '/api/login', ['json' => [
             'email' => 'rexrider75@gmail.com',
             'password' => 'testtest',
+            'address' => '4 rue de la paix',
+            'birthday' => '1990-01-01',
+            'lastname' => 'sidox',
+            'firstname' => 'sidox',
+            'token' => bin2hex(random_bytes(32)),
+            'roles' => ['ROLE_USER'],
         ]]);
 
         $this->assertResponseStatusCodeSame(200);
